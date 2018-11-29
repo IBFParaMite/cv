180d17752d21730c51082f50f97f68764aff1024
Cameron Ferns <cameron@chrisferns.co.uk>
2018-11-29 21:41:17 +0000

    Initial mvc change

---
+++ CV/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CV.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            return View();
+        }
+
+        public ActionResult Projects()
+        {
+            return View();
+        }
+
+        public ActionResult WorkExperience()
+        {
+            return View();
+        }
+
+        public ActionResult References()
+        {
+            return View();
+        }
+    }
+}
